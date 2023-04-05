25b55efde01190e50323ef89b86d8dbdcffc89c2
FaizanMohammed326 <faizan.mohammed@tibilsoultions.com>
2023-04-05 10:04:07 +0530

    added refresh token functionality

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { from, Observable } from 'rxjs';
+import { from, map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -16,6 +16,7 @@ export class AuthenticationService {
   }
 
   logout(): void {
+    this.stopRefreshTokenTimer();
     localStorage.clear();
     this._router.navigate(['/login']);
   }
@@ -27,10 +28,44 @@ export class AuthenticationService {
         'Content-Type': 'application/x-www-form-urlencoded',
       };
       const URL = `${environment.keycloakUrl}/realms/${environment.keycloakRealm}/protocol/openid-connect/token`;
-      return this._http.post(URL, payload, { headers: headers });      
+      return this._http.post(URL, payload, { headers: headers });
 
     } catch (error) {
       console.log('keyClock.impl.service', error.message);
     }
+  }
+
+  refreshToken() {
+    let refreshToken = localStorage.getItem('refresh_token')
+    let payload = `client_id=${environment.keycloakClient}&client_secret=${environment.keycloakSecret}&grant_type=refresh_token&refresh_token=${refreshToken}`
+    let headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    const URL = `${environment.keycloakUrl}/realms/${environment.keycloakRealm}/protocol/openid-connect/token`;
+    return this._http.post(URL, payload, { headers: headers }).pipe(map((res: any) => {
+      localStorage.setItem('token', res.access_token)
+      localStorage.setItem('refresh_token', res.refresh_token)
+      this.startRefreshTokenTimer();
+    }));
+  }
+
+  // helper methods
+
+  refreshTokenTimeout?: NodeJS.Timeout;
+
+  startRefreshTokenTimer() {
+    // parse json object from base64 encoded jwt token
+    const jwtBase64 = localStorage.getItem('token').split('.')[1];
+
+    const jwtToken = JSON.parse(atob(jwtBase64));
+
+    // set a timeout to refresh the token a minute before it expires
+    const expires = new Date(jwtToken.exp * 1000);
+    const timeout = expires.getTime() - Date.now() - (60 * 1000);
+    this.refreshTokenTimeout = setTimeout(() => this.refreshToken().subscribe(), timeout);
+  }
+
+  stopRefreshTokenTimer() {
+    clearTimeout(this.refreshTokenTimeout);
   }
 }
