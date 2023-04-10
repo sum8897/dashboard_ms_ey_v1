868d6453357d136f4a56c079b1600fc4e76cabaa
FaizanMohammed326 <faizan.mohammed@tibilsoultions.com>
2023-04-10 14:46:33 +0530

    environments added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  google_analytics_tracking_id:"UA-260554959-3",
+  apiURL: 'api_end_point/api/queryBuilder',
+  stateCode: "AP",
+  numberFormat: {
+    reports: {
+      locale: 'en-IN',
+      format: 'short'
+    }
+  },
+  skipCluster: true,
+  config: "state",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  google_analytics_tracking_id: "UA-260554959-3",
+  apiURL: 'http://localhost:3008',
+  stateCode: "AP",
+  numberFormat: {
+    reports: {
+      locale: 'en-IN',
+      format: 'short'
+    }
+  },
+  config: "state",
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
