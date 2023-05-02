e8b79e9df3cee572f5a9600fe6e735573301df8f
cqube-sunbird <132258144+cqube-sunbird@users.noreply.github.com>
2023-05-02 13:20:37 +0530

    Updated vercel env

--- src/environments/environment.vercel.ts
+++ src/environments/environment.vercel.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   google_analytics_tracking_id: "UA-260554959-3",
-  apiURL: 'https://cqube-apr28-demo.tibilprojects.com/api/queryBuilder',
-  stateCode: "AP",
+  apiURL: 'https://qr.staging.cqube.samagra.io',
+  stateCode: "JH",
   numberFormat: {
     reports: {
       locale: 'en-IN',
