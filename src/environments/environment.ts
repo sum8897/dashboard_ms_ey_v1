3e9f0a8bfb8521166f3e2b61c084f267008221eb
Shruti3004 <mail2shruti.ag@gmail.com>
2023-05-02 09:57:11 +0530

    fix: menu
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   google_analytics_tracking_id: "UA-260554959-3",
   apiURL: 'https://qr.staging.cqube.samagra.io',
-  stateCode: "AP",
+  stateCode: "JH",
   numberFormat: {
     reports: {
       locale: 'en-IN',
