501e34540ea5bb0505d5285c8060dce01af2a7f5
FaizanMohammed326 <faizan.mohammed@tibilsoultions.com>
2023-05-07 17:59:12 +0530

    map view expanded to full view

--- src/app/shared/components/maps/leaflet-map/leaflet-map.component.ts
+++ src/app/shared/components/maps/leaflet-map/leaflet-map.component.ts
@@ -282,7 +282,7 @@ export class LeafletMapComponent implements OnInit, AfterViewInit, OnChanges {
   }
 
   createMarkers(mapData: any, singleColor?: any): void {
-    let reportTypeIndicator = this.mapData?.options?.map && this.mapData.options.map.reportTypeIndicator ? this.mapData.options.map.reportTypeIndicator : (typeof this.mapData.data[0].indicator === 'string') ? 'boolean' : 'value'
+    let reportTypeIndicator = this.mapData?.options && this.mapData.options.reportIndicatorType ? this.mapData.options.reportIndicatorType : (typeof this.mapData.data[0].indicator === 'string') ? 'boolean' : 'value'
     if (mapData && this.level !== 'state') {
       let min!: number, max!: number, values: any[] = [];
       if (reportTypeIndicator === 'value') {
