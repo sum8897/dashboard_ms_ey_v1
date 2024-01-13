8a0781b0e03f1aa1766f3d42da1a588a0c10ec08
himanshugoel435 <himanshugoel435@gmail.com>
2024-01-13 17:32:20 +0530

    library and school-infra config subcategory remove

--- src/environments/environment.vercel.ts
+++ src/environments/environment.vercel.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   google_analytics_tracking_id: "UA-260554959-3",
   apiURL: 'https://qr.staging.cqube.samagra.io',
-  stateCode: "JH",
+  stateCode: "AS",
   numberFormat: {
     reports: {
       locale: 'en-IN',
