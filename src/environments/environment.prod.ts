f9974f9a944416368cf498fa53c35b42013a73a9
Ashish Chaurasiya <ashish.chaurasiya@in.ey.com>
2024-06-03 12:19:09 +0530

    added api in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   production: true,
   google_analytics_tracking_id:"UA-260554959-3",
   apiURL: 'api/queryBuilder',
+  apiURLOnly:'https://sanccssl.sansoftwares.com/crm/caller/Api', 
   stateCode: "AS",
   numberFormat: {
     reports: {
