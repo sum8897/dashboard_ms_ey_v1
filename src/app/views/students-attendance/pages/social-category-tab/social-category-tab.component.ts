b03f7f66c814c0dcda0cc4b1baf9014e20ff94ac
Mohanapriya777 <130358614+Mohanapriya777@users.noreply.github.com>
2025-06-18 12:41:07 +0530

    Updated code for social-category-tab

--- src/app/views/students-attendance/pages/social-category-tab/social-category-tab.component.ts
+++ src/app/views/students-attendance/pages/social-category-tab/social-category-tab.component.ts
@@ -1,15 +1,272 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  OnDestroy,
+  Component,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { RbacService } from "src/app/core/services/rbac-service.service";
+import { WrapperService } from "src/app/core/services/wrapper.service";
+import { ReportDrilldownService } from "src/app/core/services/report-drilldown/report-drilldown.service";
+import { CommonService } from "src/app/core/services/common/common.service";
+import moment from "moment";
+import { config } from "../../config/student_attendance_system_config";
+import { SocialCategoryTableComponent } from "./reports/social-category-table/social-category-tab.component";
+import { SocialCategoryGraphComponent } from "./reports/social-category-graph/social-category-graph.component";
 
 @Component({
-  selector: 'app-social-category-tab',
-  templateUrl: './social-category-tab.component.html',
-  styleUrls: ['./social-category-tab.component.scss']
+  selector: "app-social-category-tab",
+  templateUrl: "./social-category-tab.component.html",
+  styleUrls: ["./social-category-tab.component.scss"],
 })
 export class SocialCategoryTabComponent implements OnInit {
+  bigNumberReports: any = {};
+  filters: any;
+  maxDate: any;
+  minDate: any;
+  startDate: any;
+  endDate: any;
+  reportsData: any[] = [];
+  rbacDetails: any;
+  defaultSelectedDays: any = 7;
+  drillDownLevel: any = 1;
+  hasTimeSeriesFilters: boolean = false;
+  hasCommonFilters: boolean = true;
+  bigNumberMetrics: any = [];
+  criteriaConfig: any = config.criteria_config;
+  trendLineConfig = {
+    options: {
+      tooltips: { displayColors: false },
+      legend: { display: false },
+    },
+  };
+  filterValues: any;
+  drillDownSubscription: any;
+  tabLabel: any = "Social Category";
 
-  constructor() { }
+  //added for full school report download
+  // title = "Download School Report"
+  schoolReportsData: any[] = [];
+  pagereportName = "Social Category";
+  //
+  @ViewChild("social_category_table_one")
+  social_category_table_one: SocialCategoryTableComponent;
+  @ViewChild("attendanceGraph") attendanceGraph: SocialCategoryGraphComponent;
 
-  ngOnInit(): void {
+  // @ViewChild('genderTable') genderTable:  AttendanceTableComponent;
+  // @ViewChild('performingBigNumbercardone') performingBigNumbercardone: BigNumberPhotographsComponent;
+  // @ViewChild('performingBigNumbercardtwo') performingBigNumbercardtwo: BigNumberTwoComponent;
+  // @ViewChild('performingBigNumbercardthree') performingBigNumbercardthree: BigNumberThreeComponent;
+  // @ViewChild('performingBigNumbercardfour') performingBigNumbercardfour: BigNumberFourComponent;
+  // @ViewChild('performingBigNumbercardfive') performingBigNumbercardfive: BigNumberFiveComponent;
+  // @ViewChild('performingBigNumbercardsix') performingBigNumbercardsix: BigNumberSixComponent;
+  // @ViewChild('studentComparativeBigNumber') studentComparativeBigNumber: ComparativeCardComponent;
+
+  constructor(
+    private _wrapperService: WrapperService,
+    private readonly _commonService: CommonService,
+    private _rbacService: RbacService,
+    private readonly _reportDrilldownService: ReportDrilldownService
+  ) {
+    this._rbacService.getRbacDetails().subscribe((rbacDetails: any) => {
+      this.rbacDetails = rbacDetails;
+      this.drillDownLevel = rbacDetails.role;
+    });
+    this.drillDownSubscription =
+      this._reportDrilldownService.drilldownData.subscribe((data) => {
+        if (data) {
+          this.drillDownLevel = data.hierarchyLevel || this.rbacDetails?.role;
+          this.reportsData = [];
+          this.schoolReportsData = [];
+        }
+      });
   }
 
+  ngOnInit(): void {
+    // this._reportDrilldownService.emit()
+  }
+
+  ngOnDestroy(): void {
+    this._reportDrilldownService.emit("reset");
+    this.drillDownSubscription.unsubscribe();
+  }
+
+  async ngAfterViewInit(): Promise<void> {
+    if (this.hasCommonFilters) {
+      this.filters = await this._wrapperService.constructCommonFilters(
+        config.filters,
+        this.tabLabel
+      );
+      console.log("line103- filters", this.filters);
+      this.social_category_table_one?.getReportData(
+        {
+          filterneed: this.hasCommonFilters,
+          filterValues: this.filters.map((filter) => {
+            return {
+              ...filter,
+              columnName: filter.valueProp,
+              filterType: filter.id,
+            };
+          }),
+        },
+        this.startDate,
+        this.endDate
+      );
+      this.attendanceGraph?.getReportData(
+        {
+          filterneed: this.hasCommonFilters,
+          filterValues: this.filters.map((filter) => {
+            return {
+              ...filter,
+              columnName: filter.valueProp,
+              filterType: filter.id,
+            };
+          }),
+        },
+        this.startDate,
+        this.endDate
+      );
+      // this.performingBigNumbercardone?.getReportData({filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+      // this.performingBigNumbercardtwo?.getReportData({filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+      // this.performingBigNumbercardthree?.getReportData({filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+      // this.performingBigNumbercardfour?.getReportData({filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+      // this.performingBigNumbercardfive?.getReportData({filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+      // this.performingBigNumbercardsix?.getReportData({filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+
+      // this.studentComparativeBigNumber?.getReportData({filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+    } else if (this.hasCommonFilters === false) {
+      // this.loWisePerformance?.getReportData({filterneed: this.hasCommonFilters});
+    }
+    if (this.startDate === undefined && this.endDate === undefined) {
+      console.log("hello");
+      let endDate = new Date();
+      let days = endDate.getDate() - this.defaultSelectedDays;
+      let startDate = new Date();
+      startDate.setDate(days);
+      this.startDate = moment(startDate).format("YYYY-MM-DD");
+      this.endDate = moment(endDate).format("YYYY-MM-DD");
+      console.log("datess", startDate, endDate);
+
+      // this.getSchoolReportData()
+      this.updateReportsData();
+    }
+  }
+
+  checkReport(key: string, reportType: string): Boolean {
+    let reportConfig = config;
+    let flag = false;
+    reportConfig[key]?.filters?.forEach((filter: any) => {
+      if (
+        Number(filter.hierarchyLevel) === Number(this.rbacDetails?.role) &&
+        Object.keys(filter?.actions?.queries).includes(reportType)
+      ) {
+        flag = true;
+      }
+    });
+    return flag;
+  }
+
+  csvDownload(csvData: any) {
+    if (csvData) {
+      this.reportsData = [];
+      this.reportsData.push(csvData);
+    }
+  }
+
+  updateReportsData(): void {
+    console.log("dttttttt", this.filters, this.startDate, this.endDate);
+
+    this.social_category_table_one?.getReportData(
+      {
+        filterneed: this.hasCommonFilters,
+        filterValues: this.filters.map((filter) => {
+          return {
+            ...filter,
+            columnName: filter.valueProp,
+            filterType: filter.id,
+          };
+        }),
+      },
+      this.startDate,
+      this.endDate
+    );
+    this.attendanceGraph?.getReportData(
+      {
+        filterneed: this.hasCommonFilters,
+        filterValues: this.filters.map((filter) => {
+          return {
+            ...filter,
+            columnName: filter.valueProp,
+            filterType: filter.id,
+          };
+        }),
+      },
+      this.startDate,
+      this.endDate
+    );
+    // this.performingBigNumbercardone?.getReportData({ filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+    // this.performingBigNumbercardtwo?.getReportData({ filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+    // this.performingBigNumbercardthree?.getReportData({ filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+    // this.performingBigNumbercardfour?.getReportData({ filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+    // this.performingBigNumbercardfive?.getReportData({ filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+    // this.performingBigNumbercardsix?.getReportData({ filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+    // this.studentComparativeBigNumber?.getReportData({ filterneed: this.hasCommonFilters, filterValues: this.filters.map((filter) => { return { ...filter, columnName: filter.valueProp, filterType: filter.id } }) },this.startDate,this.endDate);
+  }
+  filtersUpdated(filters: any) {
+    this.reportsData = [];
+    this.filters = filters;
+    this.updateReportsData();
+  }
+
+  timeSeriesUpdated(event: any): void {
+    if (event?.startDate !== null && event?.endDate !== null) {
+      this.reportsData = [];
+      this.schoolReportsData = [];
+      this.startDate = moment(event.startDate).format("YYYY-MM-DD");
+      this.endDate = moment(event.endDate).format("YYYY-MM-DD");
+      this.updateReportsData();
+    }
+  }
+
+  schoolCsvDownload(csvData: any, hierarchyLevel: any) {
+    if (csvData && this.drillDownLevel == hierarchyLevel) {
+      this.schoolReportsData = [];
+      this.schoolReportsData.push(csvData);
+    }
+  }
+
+  getSchoolReportData(startDate?: string, endDate?: string) {
+    let query;
+    if (startDate && endDate) {
+      this.startDate = startDate;
+      this.endDate = endDate;
+    } else {
+      let endDate = new Date();
+      let days = endDate.getDate() - this.defaultSelectedDays;
+      let startDate = new Date();
+      startDate.setDate(days);
+      this.startDate = startDate?.toISOString().split("T")[0];
+      this.endDate = endDate?.toISOString().split("T")[0];
+    }
+
+    this._commonService.getReportDataNew(query).subscribe((res: any) => {
+      let d = {
+        reportData: res,
+        reportType: "map",
+        reportName: "school_type_table",
+      };
+      if (d.reportData.length > 0) {
+        this.schoolReportsData.push(d);
+      }
+    });
+  }
+
+  settimeSeriesDates(dates: any) {
+    // this.minDate = (this.minDate === undefined || (dates?.minDate && this.minDate < dates.minDate)) ? dates?.minDate : this.minDate
+    // this.maxDate = (this.maxDate === undefined || (dates?.maxDate && this.maxDate > dates.maxDate)) ? dates.maxDate : this.maxDate
+  }
+  importBigNumberMetrics(bigNumberMetric: any) {
+    this.bigNumberMetrics[bigNumberMetric.ind] = bigNumberMetric.data;
+    console.log(bigNumberMetric);
+  }
 }
